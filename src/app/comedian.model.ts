35770c287f9ad3f4e98fb5b88ccccfd80bf1bb28
eluts <eluts15@gmail.com>
2017-07-14 10:31:40 -0700

    hard code data to verify it is showing comedians as expected.

---
+++ src/app/comedian.model.ts
@@ -0,0 +1,3 @@
+export class Comedian {
+  constructor(public title: string, public name: string, public description: string, public id: number) { }
+}
--- src/app/finder/finder.component.ts
+++ src/app/finder/finder.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Comedian } from '../comedian.model';
 
 @Component({
   selector: 'app-finder',
   templateUrl: './finder.component.html',
   styleUrls: ['./finder.component.css']
 })
-export class FinderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FinderComponent {
+  comedians: Comedian[] = [
+     new Comedian("Popular Comedians", "Kevin Hart", "Makes people laugh severely.", 1),
+     new Comedian("Podcasting Elite", "Joe Rogan", "Makes people question life... and makes people laugh.", 2),
+     new Comedian("Series Regulars", "T.J. Miller", "Makes people laugh a lot", 3),
+     new Comedian("Stand-Up Legends", "Chris Rock", "Is a legend.", 4),
+     new Comedian("Stand-Up Legends", "Dave Chappelle", "Is a legend.", 5)
+  ];
 }
